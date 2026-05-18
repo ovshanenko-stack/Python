0ca7be4bb971e89719b9624c83b5c875ee824b3b
Sasha Shanenko <ovshanenko@gmail.com>
2026-05-18 16:23:53 +0300

    My third task

---
+++ Task3.py
@@ -0,0 +1,8 @@
+name = "pynative"
+print("Present an even index number in giving string", name)
+      
+even = name[0::2]
+print("Printing even chars")
+for ev in even:
+           
+        print(ev, end= " ")
