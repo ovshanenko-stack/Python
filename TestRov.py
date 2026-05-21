1603dc69ca5f68917ce7ea28ee7f8bff7260c15f
Sasha Shanenko <ovshanenko@gmail.com>
2026-05-21 18:59:47 +0300

    My Classwork1_1

--- TestRov.py
+++ TestRov.py
@@ -1,9 +1,9 @@
 #Ask user to input number
 #num = int(input("Ведіть число: "))
-name = [1,2,3,4,5,6,7,8,9,10]
+name = [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
 #Check if number is positive, negative or zero
-max = 0
-min = 0
+max = name[0]
+min = name[0]
 for i in name:
     if i > max:
         max = i
