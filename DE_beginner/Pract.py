00c7506ee53b4915114b35dcc6c33ae5e7c4df10
Sasha Shanenko <ovshanenko@gmail.com>
2026-05-21 13:03:00 +0300

    My Homework1

---
+++ DE_beginner/Pract.py
@@ -0,0 +1,50 @@
+#Task1. Greetings
+#print("Hello, Python!")
+
+#Task2.My name
+#name = 'Oleksandr'
+#print("My name is", name)
+
+#Task3.My age
+#age = 46
+#print("My age is", age)
+
+#Task4. Few prints
+#print('Я вивчаю Python')
+#print('Мені подобається програмування')
+#print('Це мій перший код')
+
+#Task5. Different data types
+#string = "Hello, World!"
+#chuslo = 46
+#drob_chuslo = 3.14
+#logik = True
+#print(string, chuslo, drob_chuslo, logik, sep = '\n')
+
+#Task6. Comments
+#string = "Hello, World!"  #Data name for string
+#chuslo = 46               #Data name for integer
+#drob_chuslo = 3.14        #Data name for float
+#logik = True              #Data name for boolean
+#print(string, chuslo, drob_chuslo, logik, sep = '\n')  #Print all data in new line
+# 
+# Task8. Correct mistake
+#not corect   print(Hello world)
+#correct 
+#print("Hello world")
+
+#Task9. Change the statements
+#city = 'Lviv'
+#city = 'Kyiv'
+#print(city)
+
+#Task10. Mini application
+#name = 'Alexander'
+#age = 46
+#is_student = "Академія для Героїв"
+#print("Імя :", name)
+#print("Вік :", age)
+#print("Навчання :", 'Студент ', is_student)
+
+#Bonus. Ukrainian text
+print('Вітання, перший заннята завершене')
---
+++ DE_beginner/main.py
@@ -0,0 +1,5 @@
+#This program for print my data
+name = 'Oleksandr'
+age = 46
+print("My name is", name)
+print("My age is", age)
